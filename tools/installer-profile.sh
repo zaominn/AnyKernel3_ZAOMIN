941a1d0008991e9066732d8949df2d300c7b9954
ZAOMIN <205947332+zaominn@users.noreply.github.com>
2026-09-14 08:48:16 -0400

    fix: identify self-use package builder

--- tools/installer-profile.sh
+++ tools/installer-profile.sh
@@ -25,7 +25,7 @@ installer_clear_legacy_ksud_attrs() {
 installer_prepare_flash() {
     ui_print " "
     ui_print "AnyKernel3 Kernel Installer"
-    ui_print "内核构建者: ${INSTALLER_BUILDER:-ZAOMI}"
+    ui_print "内核构建者: ${INSTALLER_BUILDER:-ZAOMIN}"
     ui_print " "
     installer_clear_legacy_ksud_attrs
     sync
